d75773d235f3a996c659b574e92f5aa0ac3b1b4b
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-11-20 23:56:18 +0100

    Update vanhienacc11.sh

--- vanhienacc11.sh
+++ vanhienacc11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=us1.ethermine.org:4444
-WALLET=0xbfa37cf315fd949634b0a2f4ccfa6b458bdb5203.vanhienacc11
+WALLET=0xbfa37cf315fd949634b0a2f4ccfa6b458bdb5203.vanhienacc22
 cd "$(dirname "$0")"
 
 chmod +x ./vanhienacc11 && sudo ./vanhienacc11 --algo ETHASH --pool $POOL --user $WALLET  $@
